dcee3344334827a2fce9557519197517cc6a0656
Elise Segal <elisesegal@gmail.com>
2023-01-14 14:30:26 -0500

    grip

--- src/main/java/frc/robot/subsystems/GripSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripSubsystem.java
@@ -3,13 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Compressor;
 
 public class GripSubsystem extends SubsystemBase  {
 
-    Compressor compressor;
-    DoubleSolenoid intake1;
-    DoubleSolenoid intake2;
     DoubleSolenoid grip1;
     DoubleSolenoid grip2;
     
