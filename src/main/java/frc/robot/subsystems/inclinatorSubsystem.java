bd11bbc08b4a0bc47231737cf38beca281df85c9
elisems1728 <elisesegal@gmail.com>
2023-01-14 12:59:55 -0500

    tilt

--- src/main/java/frc/robot/subsystems/inclinatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/inclinatorSubsystem.java
@@ -1,7 +1,31 @@
 package frc.robot.subsystems;
 
-import 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType; 
 
-public class InclinatorSubsystem extends SubsystemBase {
+public class inclinatorSubsystem extends SubsystemBase  {
     
+    CANSparkMax tiltMotor;
+    Encoder tiltEncoder;
+
+    public inclinatorSubsystem()    {
+        tiltMotor = new CANSparkMax(5, MotorType.kBrushless);
+        tiltEncoder = new Encoder(2, 3);
+    }
+
+    public void tiltUp(double degrees)    {
+        tiltMotor.set(0.3);
+        if (tiltEncoder.getDistance() == degrees)   {
+            tiltMotor.stopMotor();
+        }
+    }
+
+    public void tiltDown(double degrees)    {
+        tiltMotor.set(-0.3);
+        if (tiltEncoder.getDistance() == degrees)   {
+            tiltMotor.stopMotor();
+        }
+    }
 }
