c8a198af2718960e3f68098dc4b78de42382f214
Elise Segal <elisesegal@gmail.com>
2023-01-17 17:31:22 -0500

    pushing changes from Saturday

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -54,7 +54,7 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {}
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  //@Override
+  // @Override
   // public void autonomousInit() {
   //   m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
--- src/main/java/frc/robot/subsystems/GripSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
 
 public class GripSubsystem extends SubsystemBase  {
 
